54b692d258416af9a45ca9244339eb7dc6735fdf
Shiyao <liushiyao2014@gmail.com>
2017-12-09 11:15:42 -0600

    add index

--- first_app/views.py
+++ first_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+# Each View create at least one variable
+def index(request):
+    """
+    type: request
+    """
+    return HttpResponse("Hello World!")
+
